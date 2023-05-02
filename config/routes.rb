42d020764739e0b6466983ce1cbad34eba77fae4
dom1ners <toxic.dominers@gmail.com>
2023-05-02 14:28:51 +0300

    blog_2

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root "posts#index"
+
+  get "/posts", to: "posts#index"
+  
+  resources :posts do
+    resources :comments
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
